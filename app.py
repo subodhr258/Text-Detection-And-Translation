3449eab6f938a75e986071776233fdd8ef1bf772
Subodh Rajpopat <subodhr2001@gmail.com>
2022-03-24 15:00:16 +0530

    Flask-Deployment

--- app.py
+++ app.py
@@ -1,6 +1,15 @@
 from flask import Flask, request, jsonify, render_template
 import pandas as pd
 from werkzeug.utils import secure_filename
+import easyocr
+import cv2
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+from googletrans import Translator
+from Languages import languages
+
+translator = Translator()
 
 app = Flask(__name__)
 
@@ -13,11 +22,35 @@ def predict():
     '''
     For rendering results on HTML GUI
     '''
-    output = []
+    lang = request.form.get('languages')
     if request.method == 'POST':
-        pass
+        file = request.files['file']
+        filename = secure_filename('input.jpg')
+        file.save(filename)
+        
+        from_language = languages[lang]
+        image = cv2.imread('./input.jpg')
+        reader = easyocr.Reader(['en',from_language])
+        res = reader.readtext('./input.jpg')
+        
+        for (bbox, text, prob) in res: 
+            if translator.detect(text).lang!='en':
+                (tl, tr, br, bl) = bbox
+                tl = (int(tl[0]), int(tl[1]))
+                tr = (int(tr[0]), int(tr[1]))
+                br = (int(br[0]), int(br[1]))
+                bl = (int(bl[0]), int(bl[1]))
+                cv2.rectangle(image, tl, br, (0, 255, 0), 2)
+                translation = translator.translate(text).text
+                cv2.putText(image, translation, (tl[0], tl[1] - 10),
+                cv2.FONT_HERSHEY_SIMPLEX, 0.8, (255, 0, 0), 2)
+
+        plt.rcParams['figure.figsize'] = (16,16)
+        plt.axis('off')
+        plt.imshow(image)
+        plt.savefig('static/output.png',bbox_inches='tight', pad_inches=0)
     
-    return render_template('index.html',image="")
+    return render_template('index.html',image=True)
 
 if __name__ == "__main__":
     app.run()
